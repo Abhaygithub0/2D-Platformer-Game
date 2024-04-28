78318669ed36455a996e6116a2b1712a6e45e204
DESKTOP-9K53R7V\MSI <abhaytiwariwork0@gmail.com>
2024-04-28 11:48:00 +0530

    commit
    fixed the code of jump and crouch part

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,24 +1,47 @@
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public Animator animator;
+    [SerializeField] private Animator animator;
+    [SerializeField] private BoxCollider2D boxCollider2D;
+    [SerializeField] private SpriteRenderer spr;
 
-    void Update()
+    private Vector2 initialBoxSize;
+    private Vector2 initialBoxOffset;
+
+    private Color colore;
+
+    
+
+ 
+
+ void Start()
+ {
+     initialBoxSize = boxCollider2D.size;
+        initialBoxOffset = boxCollider2D.offset;
+        colore = spr.color;
+ }
+    private void Update()
     {
-        float speedkivalue = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKey(KeyCode.Q)){
+            spr.color=Color.blue;
+        }
+        else {
+spr.color=colore;
+        }
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+          PlayJumpAnimation( verticalInput );
 
-        animator.SetFloat("Speed", Mathf.Abs(speedkivalue));
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+        
+
         Vector3 scale = transform.localScale;
-        if (speedkivalue < 0)
+        if (horizontalInput < 0)
         {
             scale.x = -1 * Mathf.Abs(scale.x);
         }
-        else if (speedkivalue > 0)
+        else if (horizontalInput > 0)
         {
             scale.x = 1 * Mathf.Abs(scale.x);
         }
@@ -26,18 +49,45 @@ public class playerController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftControl))
         {
-            Debug.Log("key pressed");
-            bool isCrouching = animator.GetBool("IsCrouch");
-            animator.SetBool("IsCrouch", ! isCrouching);
+            
+            Crouch(true);
         }
-
- float jumpvalue = Input.GetAxisRaw("Vertical");
-  animator.SetBool("IsJump", false);
-      
-        if (jumpvalue > 0)
+        else
         {
-           animator.SetBool("IsJump", true);
+            Crouch(false);
         }
+    }
+
+    public void Crouch(bool crouch)
+    {
        
+        if (crouch==true)
+        {
+             animator.SetBool( "IsCrouch", crouch );
+             float offX = 0.01648927f;     //Offset X
+            float offY = 0.55f;      //Offset Y
+
+            float sizeX = 0.5792472f;     //Size X
+            float sizeY = 1f;     //Size Y
+
+            boxCollider2D.size = new Vector2( sizeX, sizeY );  
+            boxCollider2D.offset = new Vector2( offX, offY );   
+           
+        }
+        else
+        {
+            boxCollider2D.size = initialBoxSize;
+            boxCollider2D.offset = initialBoxOffset;
+        }
+    }
+
+     public void PlayJumpAnimation( float vertical )
+    {
+        if ( vertical > 0 )
+        {
+            animator.SetTrigger( "IsJump" );
+        }
     }
 }
+
+
